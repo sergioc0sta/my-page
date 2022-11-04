dacf08884351334cbad4831b797c7df9381d087f
kostyny <kostyny@gmail.com>
2022-11-04 11:09:18 +0000

    feat(my-page): New version with new plugins

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import type { HeadFC } from "gatsby"
+
+
+const IndexPage = () => {
+  return (
+   <p>yo yo</p>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
